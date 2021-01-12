161d576dc02905d2cc9ab80aba870ac2c27ed0d1
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-12 17:01:47 +0900

    GET 요청에 응답하기

--- src/main/java/com/example/demo/controller/GetController.java
+++ src/main/java/com/example/demo/controller/GetController.java
@@ -1,6 +1,38 @@
 package com.example.demo.controller;
 
 
+    import com.example.demo.model.SearchParam;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestMethod;
+    import org.springframework.web.bind.annotation.RequestParam;
+    import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // localhost:8080/api
 public class GetController {
 
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
+    public String getRequest() {
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+        System.out.println("id : " + id);
+        System.out.println("password : " + password);
+
+        return id + password;
+    }
+
+    // localhost:8080/api/getMultiParameter?account=abcd&email=study@gmail.com&page=10
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam) {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        // { "account" : "", "email" : "", "page" : 0 }
+        return searchParam;
+    }
 }
